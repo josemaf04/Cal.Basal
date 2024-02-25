72dc5071be55d3856fce784917adeea22503a705
josemaf04 <160612114+josemaf04@users.noreply.github.com>
2024-02-25 15:33:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ console.log("hola mundo")
 const PESO = document.getElementById("peso");
 
 let peso = (PESO.value);
+let resultado = 0;
+let flu = document.getElementById('flu');
+let man = document.getElementById('man'); 
 
 const BOTON = document.getElementById("calcular");
 BOTON.addEventListener("click", calcularFlujo)
@@ -10,20 +13,28 @@ function calcularFlujo(){
     console.log("click en el botón");
     let peso = (PESO.value);
     console.log(peso);
-    holiday()
+    holiday(peso)
 }
 
-function holiday(){
-    if (PESO.value <=10){
-        resultado =parseInt (PESO.value) * 100;
+function holiday(peso){
+    
+    if (peso <=10){
+        resultado =parseInt(peso) * 100;
         console.log(resultado);
     }
-    else if (PESO.value >10 && PESO.value <=20) {;
-        resultado =parseInt (((PESO.value - 10) * 50)+1000);
+    else if (peso >10 && peso <=20) {;
+        resultado = (((parseInt(peso) - 10) * 50)+1000);
         console.log(resultado);
     }
-    else if (PESO.value >20) {
-        resultado=parseInt (((PESO.value - 20)*20)+1500);
+    else if (peso >20) {
+        resultado= (((parseInt (peso) - 20)*20)+1500);
         console.log(resultado);
     }
+    let mantenimiento = (resultado / 24) * 1.5;
+    flu.innerHTML = `${resultado}cc/h`
+    man.innerHTML = `m+m/2: ${mantenimiento}cc/h`
+    man.style.display = "block"
+    flu.style.display = "block"
+
+    
 }
